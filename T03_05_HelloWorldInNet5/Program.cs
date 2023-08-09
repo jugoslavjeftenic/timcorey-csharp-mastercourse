c433cc111086a7de68b7981aa5996b110871d37e
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-09 17:09:31 +0200

    HelloWorldInNet5

---
+++ T03_05_HelloWorldInNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T03_05_HelloWorldInNet5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
