8a35bb601235859317edbf25fab9462146e82462
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-10 23:39:07 +0200

    IntVariables

---
+++ T05_03_IntVariables/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int age = 0;
+
+age = 51;
+Console.WriteLine(age);
+
+int ageInTenYears = age + 10;
+Console.WriteLine(ageInTenYears);
+
+Console.WriteLine(age / 2);
+
+// Singned Int32
+Console.WriteLine($"Int32 min [{Int32.MinValue}], max [{Int32.MaxValue}]");
+
+// Unsingned Int32
+Console.WriteLine($"UInt32 min [{UInt32.MinValue}], max [{UInt32.MaxValue}]");
