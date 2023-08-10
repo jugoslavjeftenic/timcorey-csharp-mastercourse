56c283f48f401bd7cda2692aab9dd41f5d23b3e1
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-10 15:48:14 +0200

    GreetingApp

---
+++ T04_02_GreetingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Welcome User To App
+Console.WriteLine("Welcome to Greeting Application!");
+Console.WriteLine("--------------------------------");
+Console.WriteLine();
+
+// Ask for First Name
+Console.Write("What is your first name? ");
+string? firstName = Console.ReadLine();
+Console.WriteLine("--------------------------------");
+Console.WriteLine();
+
+// Greet user by name
+Console.WriteLine($"Hello {firstName}!");
+Console.WriteLine("Thank you for using aplication.");
+Console.ReadLine();
