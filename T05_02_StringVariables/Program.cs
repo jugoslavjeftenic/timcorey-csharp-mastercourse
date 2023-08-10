cf9c75ea7ebb2fed050ab87109a4be50150552c4
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-10 22:55:04 +0200

    StringVariables

---
+++ T05_02_StringVariables/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+string firstName = string.Empty;
+string lastName = string.Empty;
+
+firstName = "Jugoslav";
+// firstName = "123";
+lastName = "Jeftenic";
+
+Console.WriteLine(firstName + " " + lastName);
+
+// String interpolation
+Console.WriteLine($"Hello {firstName} {lastName}");
+
+string filePath1 = "C:\\Temp\\Demo";
+// String literal - Verbatim text
+string filePath2 = @"C:\Temp\Demo";
+
+Console.WriteLine(filePath1);
+Console.WriteLine(filePath2);
+
+string testString = $@"The file for {firstName} is at C:\SampleFiles";
+
+Console.WriteLine(testString);
