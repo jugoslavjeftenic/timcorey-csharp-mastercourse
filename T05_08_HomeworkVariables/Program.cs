a012a7bd0f6b8343223e9a5dc025e93d159596b0
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-11 16:24:40 +0200

    HomeworkVariables

---
+++ T05_08_HomeworkVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+// Create a Console Application that has variables to hold a person's name,
+// age, if they are alive, and their phone number.
+// You do not need to capture these values from the user.
+
+string name = "Jugoslav";
+int age = 52;
+bool isAlive = true;
+string phoneNumber = "(024) 123-4567";
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(isAlive);
+Console.WriteLine(phoneNumber);
