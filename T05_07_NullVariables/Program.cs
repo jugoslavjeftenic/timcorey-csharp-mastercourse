7e8720fb41958013752c38bd8a5221e1e9b93f93
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-11 16:09:03 +0200

    NullVariables

---
+++ T05_07_NullVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int? age = null;
+bool? isBirthday = null;
+double? battingAverage = null;
+decimal? accountBalance = null;
+string? firstName = null;
+
+age = 0;
+
+Console.WriteLine("int: " + age);
+Console.WriteLine("bool: " + isBirthday);
+Console.WriteLine("double: " + battingAverage);
+Console.WriteLine("decimal: " + accountBalance);
+Console.WriteLine("string: " + firstName);
