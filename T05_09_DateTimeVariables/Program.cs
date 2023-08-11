6ec4e9b62d02e4013b792158aec0fd5881fcb00b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-11 17:18:03 +0200

    DateTimeVariables

---
+++ T05_09_DateTimeVariables/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.Globalization;
+
+DateTime today = DateTime.Now;
+DateTime todayUtc = DateTime.UtcNow;
+
+Console.WriteLine(today);
+Console.WriteLine(todayUtc);
+Console.WriteLine(today.ToString(format: "d"));
+Console.WriteLine(today.ToString(format: "t"));
+Console.WriteLine(today.ToString(format: "MMM"));
+Console.WriteLine(today.ToString(format: "MMMM"));
+Console.WriteLine(today.ToString(format: "MMMM dd, yyyy hh:mm tt zzz"));
+
+DateTime birthdayParse = DateTime.Parse(s: "15/6/1999");
+Console.WriteLine(birthdayParse.ToString());
+
+DateTime birthdayParseExact = DateTime.ParseExact(s: "1999/6/16", format: "yyyy/M/d", CultureInfo.InvariantCulture);
+Console.WriteLine(birthdayParseExact.ToString());
