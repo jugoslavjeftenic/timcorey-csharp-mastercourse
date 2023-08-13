acb5bf63efec8fd6f3766eef59292df42360c517
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-13 11:28:46 +0200

    DateOnlyVariables

---
+++ T05_10_DateOnlyVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+DateTime today = DateTime.Now;
+
+DateOnly birthday = DateOnly.Parse("16/9/1972");
+
+Console.WriteLine(birthday.ToString("dd MMMM yyyy"));
+
+Console.WriteLine($"Today full format: {today}");
+Console.WriteLine($"Today just date: {today.Date}");
+Console.WriteLine($"Birthday full format: : {birthday}");
