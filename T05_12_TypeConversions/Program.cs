56ba80f6a4a787b5a9ac6fe504dc85e470eee519
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-13 12:02:44 +0200

    TypeConversions

---
+++ T05_12_TypeConversions/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.Write("What is your age: ");
+string? ageText = Console.ReadLine();
+
+Console.WriteLine(ageText + 10);
+
+//int age = int.Parse(ageText);
+//bool isValidInt = int.TryParse(ageText, out int age);
+int age;
+bool isValidInt = int.TryParse(ageText, out age);
+
+Console.WriteLine($"This is valid: {isValidInt}. The number was {age}.");
+
+Console.WriteLine(age + 10);
+
+
+double testDouble = age;
+decimal testDecimal = (decimal)testDouble;
+
+Console.WriteLine(testDecimal);
