2e3eed87afd02cffe8b1bfd5ce93594b39e7afa9
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-13 11:35:54 +0200

    TimeOnlyVariables

---
+++ T05_11_TimeOnlyVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+TimeOnly opensAt = TimeOnly.Parse("8:00");
+
+TimeOnly rightNow = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine(opensAt);
+Console.WriteLine(rightNow);
+
+Console.WriteLine(TimeOnly.MinValue);
+Console.WriteLine(TimeOnly.MaxValue);
