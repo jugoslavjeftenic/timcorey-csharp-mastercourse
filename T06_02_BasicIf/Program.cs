1abd4edb067c373b863487e2c337ee31054ed1f8
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-15 12:20:23 +0200

    BasicIf

---
+++ T06_02_BasicIf/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+bool isComplete = false;
+
+if (isComplete)
+{
+    Console.WriteLine("The statement was true.");
+}
+else
+{
+    Console.WriteLine("The statement was false.");
+}
+Console.WriteLine();
+
+Console.Write("What is your first name? ");
+string? firstName = Console.ReadLine();
+
+if (firstName?.ToLower() == "Tim".ToLower())
+{
+    Console.WriteLine("Hello Mr. Corey.");
+}
+else
+{
+    Console.WriteLine($"Hello {firstName}.");
+}
+
+Console.WriteLine("End of program.");
