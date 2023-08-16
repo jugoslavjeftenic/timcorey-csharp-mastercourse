882d6bae99a75a6dc1bee595057ba4938414bcc6
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-16 10:05:55 +0200

    DoLoop

---
+++ T08_02_DoLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+bool isValidAge;
+int age;
+
+do
+{
+    Console.Write("What is your age? ");
+    isValidAge = int.TryParse(Console.ReadLine(), out age);
+
+    if (isValidAge == false)
+    {
+        Console.WriteLine("That was an invalid age.");
+    }
+} while (isValidAge == false);
+
+Console.WriteLine($"Your age is {age}.");
+
+
+
+int testNumber = 0;
+
+do
+{
+    Console.WriteLine(testNumber);
+    testNumber += 3;
+} while (testNumber < 10);
