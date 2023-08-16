0132691e6ba8be12f4437157ce36a7a676f602e3
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-16 19:47:52 +0200

    HomeworkDictionaries

---
+++ T08_09_HomeworkDictionaries/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+// Create s Dictionaries list of employee IDs and the name that goes with ID.
+// Fill in a few records.
+// Then ask the user for their ID and return their name.
+
+Dictionary<int, string> employee = new()
+{
+    [100] = "Pera",
+    [101] = "Mika",
+    [102] = "Zika",
+    [103] = "Cika",
+    [104] = "Prika",
+    [105] = "Strika",
+    [106] = "Paprika"
+};
+
+Console.Write("Please enter your ID: ");
+bool isInt = int.TryParse(Console.ReadLine(), out int ID);
+
+if (isInt)
+{
+    Console.WriteLine($"Your name is {employee[ID]}.");
+}
