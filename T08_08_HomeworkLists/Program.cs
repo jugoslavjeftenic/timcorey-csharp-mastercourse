4dd1dab1ea36fb5e799e86b0f897e7cc150f8060
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-16 13:32:57 +0200

    HomeworkLists

---
+++ T08_08_HomeworkLists/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+// Add students to a class roster List until there are no more students.
+// Then print out the count of the students to the Console.
+
+List<string> students = new();
+
+do
+{
+    Console.Write("Please add students first name to he list (type \"exit\" to exit): ");
+    string? student = Console.ReadLine();
+
+    if (student?.ToLower() == "exit")
+    {
+        break;
+    }
+
+    if (student == null || student.Length < 2)
+    {
+        Console.WriteLine("Please enter name with at least two characters.");
+    }
+    else
+    {
+        students.Add(student);
+    }
+
+} while (true);
+
+Console.WriteLine($"Total number of sutdents: {students.Count}");
