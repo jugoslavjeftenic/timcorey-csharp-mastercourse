ff1e6ceb844a28d75c99f72ac69df12ed65d7b6b
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-16 13:11:44 +0200

    Lists

---
+++ T08_06_Lists/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+List<string> firstNames = new List<string>();
+
+firstNames.Add("Tim");
+firstNames.Add("Sue");
+firstNames.Add("Bob");
+firstNames.Add("Jane");
+
+Console.WriteLine(firstNames[firstNames.Count - 1]);
+
+List<int> ages = new List<int>();
+
+ages.Add(1);
+ages.Add(2);
+ages.Add(3);
+
+string csvData = "Coreyy,Smith,Jones";
+List<string> lastNames = csvData.Split(',').ToList();
+lastNames.Add("Frenklin");
