f9f382210d49fb392f92354bb9287b63a8175450
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-17 17:34:42 +0200

    HomeworkMethods

---
+++ T09_08_HomeworkMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+// Create a welcome method, a method to ask for the user's name,
+// and another to tell that user "Hello <name>".
+// Call this methods from Program.cs.
+
+using T09_08_HomeworkMethods;
+
+UserMessages.Welcome();
+Console.WriteLine(UserMessages.HelloUser(UserMessages.GetUserName()));
---
+++ T09_08_HomeworkMethods/UserMessages.cs
@@ -0,0 +1,22 @@
+﻿
+namespace T09_08_HomeworkMethods
+{
+    public static class UserMessages
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to Application.");
+        }
+
+        public static string? GetUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        public static string HelloUser(string? name)
+        {
+            return $"Hello {name}.";
+        }
+    }
+}
