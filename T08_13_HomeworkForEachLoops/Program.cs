40d23ba07f55c308e987c52a093c745e9aab8e73
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-17 09:00:42 +0200

    HomeworkForEachLoops

---
+++ T08_13_HomeworkForEachLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+// Ask the user for their first name.
+// Continue asking for first names until there are no more.
+// Then loop through each name using foreach and tell each person hello on the Console.
+
+List<string> firstNames = new();
+
+do
+{
+    Console.Write("Enter your first name (type \"exit\" to exit): ");
+    string? readLine = Console.ReadLine();
+
+    if (readLine?.ToLower() == "exit")
+    {
+        Console.WriteLine("End of program.");
+        break;
+    }
+
+    if (readLine == null || readLine.Length < 2)
+    {
+        Console.WriteLine("Please enter name with min two characters.");
+        continue;
+    }
+
+    firstNames.Add(readLine);
+} while (true);
+
+foreach (var firstName in firstNames)
+{
+    Console.WriteLine($"Hello {firstName}.");
+}
