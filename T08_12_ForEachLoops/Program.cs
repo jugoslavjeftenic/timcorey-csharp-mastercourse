54072b39ede9547ecbe9edaf5746d4de81ef87cf
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-17 08:47:21 +0200

    ForEachLoops

---
+++ T08_12_ForEachLoops/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+string csvData = "Tim,Sue,Bob,Jane";
+List<string> firstNames = csvData.Split(',').ToList();
+
+foreach (string firstName in firstNames)
+{
+    Console.WriteLine(firstName);
+}
