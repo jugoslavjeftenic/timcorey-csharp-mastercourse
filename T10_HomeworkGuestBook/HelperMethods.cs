24a6efc4d351ecdadcb7f44fc9a5da2e0be3abdd
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-08-18 12:14:05 +0200

    HomeworkGuestBook

---
+++ T10_HomeworkGuestBook/HelperMethods.cs
@@ -0,0 +1,80 @@
+﻿
+namespace T10_HomeworkGuestBook
+{
+    public static class HelperMethods
+    {
+        public static (string guestName, int numberOfPartyMembers) GetGuestNameAndNumberOfPartyMembers()
+        {
+            string? guestName;
+            int numberOfPartyMembers;
+
+            do
+            {
+                Console.Write("Please, tell us your name? ");
+                guestName = Console.ReadLine();
+
+                if (guestName == null || guestName.Length < 2)
+                {
+                    Console.WriteLine("Your name has to have at least two characters.");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            do
+            {
+                Console.Write("Please, tell us how many guests do you have? ");
+
+                if (int.TryParse(Console.ReadLine(), out numberOfPartyMembers) == false ||
+                    numberOfPartyMembers < 0)
+                {
+                    Console.WriteLine("Please enter valid number of guests.");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+
+            return (guestName, numberOfPartyMembers);
+        }
+
+        public static bool AddAnotherGuest()
+        {
+            do
+            {
+                Console.Write("Add another guest (y/n)?");
+                string? addAnother = Console.ReadLine();
+
+                if (addAnother?.ToLower() == "y" || addAnother?.ToLower() == "yes")
+                {
+                    return true;
+                }
+
+                if (addAnother?.ToLower() == "n" || addAnother?.ToLower() == "no")
+                {
+                    return false;
+                }
+
+                Console.WriteLine("Please type (Y) or (N).");
+            } while (true);
+        }
+
+        public static void PrintGuestList(Dictionary<string, int> guestList)
+        {
+            Console.WriteLine("Guest list:");
+
+            foreach (var guest in guestList)
+            {
+                Console.WriteLine($"guest: {guest.Key}, number of people in their party {guest.Value}.");
+            }
+        }
+
+        public static int TotalNumberOfGuests(Dictionary<string, int> guestList)
+        {
+            return guestList.Sum(x => x.Value);
+        }
+    }
+}
---
+++ T10_HomeworkGuestBook/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+// Build a Console Guest Book.
+// Ask the user for their name and how many are in their party.
+// Keep track of how many people are at the party.
+// At the end, print out the guest list and the total number of guests.
+
+using T10_HomeworkGuestBook;
+
+Dictionary<string, int> guestList = new();
+
+Console.WriteLine("Welcome to Guest Book Application.");
+
+do
+{
+    var (guestName, numberOfPartyMembers) = HelperMethods.GetGuestNameAndNumberOfPartyMembers();
+    guestList.Add(guestName, numberOfPartyMembers);
+
+    if (HelperMethods.AddAnotherGuest() == false)
+    {
+        break;
+    }
+} while (true);
+
+Console.WriteLine("--------------------------------------");
+HelperMethods.PrintGuestList(guestList);
+
+Console.WriteLine("--------------------------------------");
+Console.WriteLine($"Total number of guests: {HelperMethods.TotalNumberOfGuests(guestList)}.");
